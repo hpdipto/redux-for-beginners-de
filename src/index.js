cbbccd40af8277b5e78e857c2d810d3d26a73351
Haris Pasha Dipto <haris.dipto@gmail.com>
2020-09-01 19:55:56 +0600

    Basic action implemented

--- src/index.js
+++ src/index.js
@@ -4,6 +4,51 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { createStore } from 'redux';
+
+
+// STORE -> GLOBALIZED STATE
+
+
+// ACTION  | INCREMENT
+const increment = () => {
+	return {
+		type: 'INCREMENT'
+	}
+}
+
+const decrement = () => {
+	return {
+		type: 'INCREMENT'
+	}
+}
+
+
+// REDUCER
+const counter = (state = 0, action) => {
+	switch(action.type) {
+		case "INCREMENT":
+			return state + 1;
+		case "DECREMENT":
+			return state - 1;
+		default:
+			return state;
+	}
+}
+
+
+let store = createStore(counter);
+
+// Display it in console
+store.subscribe(() => console.log(store.getState()));
+
+
+// DISPATCH
+store.dispatch(increment());
+
+
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
